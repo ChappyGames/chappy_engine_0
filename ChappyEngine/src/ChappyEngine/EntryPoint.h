453c5f2831c7a1527da4e13e42b644f0e5e370b9
ChappyGames <55817314+ChappyGames@users.noreply.github.com>
2019-09-26 21:23:18 -0500

    Implemented Log System
    Added spdlog as a git module and wrapped it into our new log system.

--- ChappyEngine/src/ChappyEngine/EntryPoint.h
+++ ChappyEngine/src/ChappyEngine/EntryPoint.h
@@ -7,6 +7,11 @@ extern ChappyEngine::Application* ChappyEngine::CreateApplication();
 int main(int argc, char** argv) {
 
 	printf("Chappy Engine V0\n");
+
+	ChappyEngine::Log::Init();
+	CE_CORE_WARN("Initialized Log!");
+	CE_INFO("Welcome to Chappy Engine's Sandbox Application!");
+
 	auto lApp = ChappyEngine::CreateApplication();
 	lApp->Run();
 	delete lApp;
---
+++ ChappyEngine/src/ChappyEngine/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace ChappyEngine {
+	class CHAPPY_API Log {
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define CE_CORE_TRACE(...)::ChappyEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CE_CORE_INFO(...)::ChappyEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CE_CORE_WARN(...)::ChappyEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CE_CORE_ERROR(...)::ChappyEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CE_CORE_FATAL(...)::ChappyEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define CE_TRACE(...)::ChappyEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CE_INFO(...)::ChappyEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CE_WARN(...)::ChappyEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CE_ERROR(...)::ChappyEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CE_FATAL(...)::ChappyEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+// Because we define these as macros, we can actually define the macro as nothing which will strip the logs from distribution builds!
+
